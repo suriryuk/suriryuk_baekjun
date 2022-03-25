7cd1b472e4884e8652ee9a89d4e0b5c294dcf587
suriryuk <63911458+suriryuk@users.noreply.github.com>
2022-03-25 20:21:47 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,5 @@
+a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+st = input()
+
+for i in a:
+    print(st.find(i), end=" ")
