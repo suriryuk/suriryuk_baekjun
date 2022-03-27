567a496afe19b32aa77d7c46de63ecb0ea979b25
suriryuk <63911458+suriryuk@users.noreply.github.com>
2022-03-27 23:47:56 +0900

    [Bronze I] Title: 단어 공부, Time: 96 ms, Memory: 32816 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,20 @@
+string = input()
+big_count = []
+many = {"alphabet" : "A", "count" : 0}
+
+string = string.upper()
+
+for i in range(65, 91):
+    count = string.count(chr(i))
+    if count != 0:
+        big_count.append(count)
+        if count > many["count"]:
+            many["alphabet"] = chr(i)
+            many["count"] = count
+
+alphabet = ord(many["alphabet"])
+
+if big_count.count(many["count"]) > 1:
+    print("?")
+else:
+    print(many["alphabet"])
