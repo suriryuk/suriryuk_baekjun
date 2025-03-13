b3b991506b4e05d808a8ec26f2bef6c05307783a
suriryuk <63911458+suriryuk@users.noreply.github.com>
2025-03-13 16:47:25 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,24 +1,30 @@
 a, b, c = map(int, input().split())
 
-if a == b and b == c:
-    print(10000 + a * 1000)
-elif a == b or a == c:
-    print(1000 + a * 100)
-elif c == b:
-    print(1000 + c * 100)
+result = 0
+if a == b:
+    if b == c:
+        # 모두 같을 때
+        result = 10000 + a * 1000
+    else:
+        # 2개 같은 눈일 때 ( a == b != c )
+        result = 1000 + a * 100
+elif a == c:
+    if a != b:
+        # 2개 같은 눈일 때 ( a == c, a != b, b != c )
+        result = 1000 + a * 100
+elif b == c:
+    if a != b:
+        # 2개 같은 눈일 때 ( a != b == c )
+        result = 1000 + b * 100
 else:
     if a > b:
         if a > c:
-            print(a * 100)
-        elif a < c:
-            print(c * 100)
-    elif b > a:
-        if b > c:
-            print(b * 100)
-        elif b < c:
-            print(c * 100)
-    elif c > a:
-        if c > b:
-            print(c * 100)
-        elif c < b:
-            print(b * 100)
+            result = a * 100
+        else:
+            result = c * 100
+    elif b > c:
+        result = b * 100
+    else:
+        result = c * 100
+
+print(result)
