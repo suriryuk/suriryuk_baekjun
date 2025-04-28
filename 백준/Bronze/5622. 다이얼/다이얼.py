e49812664b93343464010a74aae2cb1c59297c1a
suriryuk <63911458+suriryuk@users.noreply.github.com>
2025-04-28 18:36:14 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,22 +1,20 @@
-number = input()
-total = 0
+dial_chars = {
+    1 : '',
+    2 : 'ABC',
+    3 : 'DEF',
+    4 : 'GHI',
+    5 : 'JKL',
+    6 : 'MNO',
+    7 : 'PQRS',
+    8 : 'TUV',
+    9 : 'WXYZ'
+}
 
-for i in number:
-    if i in "ABC":
-        total += 3
-    elif i in "DEF":
-        total += 4
-    elif i in "GHI":
-        total += 5
-    elif i in "JKL":
-        total += 6
-    elif i in "MNO":
-        total += 7
-    elif i in "PQRS":
-        total += 8
-    elif i in "TUV":
-        total += 9
-    elif i in "WXYZ":
-        total += 10
-        
-print(total)
+dial = input()
+
+time = 0
+for ch in dial:
+    for key, value in dial_chars.items():
+        if ch in value:
+            time += (key + 1)
+print(time)
